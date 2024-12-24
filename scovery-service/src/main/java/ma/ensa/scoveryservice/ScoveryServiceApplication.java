b5ec27bd0de5c295600ee97c15c2aac058bc4d81
OuyoussMeryem <ouyss.meryem@gmail.com>
2024-12-24 11:25:15 +0100

    microservice

---
+++ scovery-service/src/main/java/ma/ensa/scoveryservice/ScoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.scoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoveryServiceApplication.class, args);
+    }
+
+}
---
+++ scovery-service/src/test/java/ma/ensa/scoveryservice/ScoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.scoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
